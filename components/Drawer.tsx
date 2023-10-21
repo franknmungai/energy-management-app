4af36eed35c80725962af594aa33f7b20f0d6fbc
Collins Kesuibai <collinskiprop13@gmail.com>
2023-10-21 21:50:09 +0300

    #[update]added pages

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,31 +1,32 @@
-import { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Toolbar from '@mui/material/Toolbar';
-import { Inter } from 'next/font/google';
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import { Inter } from "next/font/google";
 
 import {
   BiSolidDashboard,
   BiMenu,
   BiBarChartSquare,
   BiUser,
-} from 'react-icons/bi';
-import { RxDashboard, RxRocket } from 'react-icons/rx';
-import { HiOutlineDocumentReport } from 'react-icons/hi';
-import { MdOutlineDevices } from 'react-icons/md';
-import { RiSurveyLine } from 'react-icons/ri';
-import { FcAreaChart, FcElectricity } from 'react-icons/fc';
+} from "react-icons/bi";
+import { RxDashboard, RxRocket } from "react-icons/rx";
+import { HiOutlineDocumentReport } from "react-icons/hi";
+import { MdOutlineDevices } from "react-icons/md";
+import { RiSurveyLine } from "react-icons/ri";
+import NextLink from "next/link";
+import { FcAreaChart, FcElectricity } from "react-icons/fc";
 
-import Chart1 from './Chart1';
+import Chart1 from "./Chart1";
 
 const drawerWidth = 280;
 
@@ -35,25 +36,26 @@ interface Props {
    * You won't need it on your project.
    */
   window?: () => Window;
+  children?: React.ReactNode;
 }
 
 const drawerItems = [
-  [<RxDashboard />, 'Dashboard'],
-  [<BiBarChartSquare />, 'Analytics'],
-  [<MdOutlineDevices />, 'Loads'],
-  [<HiOutlineDocumentReport />, 'Report'],
-  [<RxRocket />, 'Recommendations'],
+  [<RxDashboard />, "Dashboard", "/"],
+  [<BiBarChartSquare />, "Analytics", "/analytics"],
+  [<MdOutlineDevices />, "Loads", "/loads"],
+  [<HiOutlineDocumentReport />, "Report", "/report"],
+  [<RxRocket />, "Recommendations", "/recommendations"],
 ];
 
 const secondaryItems = [
-  [<RiSurveyLine />, 'Data entry'],
-  [<BiUser />, 'User Account'],
+  [<RiSurveyLine />, "Data entry", "/data_entry"],
+  [<BiUser />, "User Account", "/account"],
 ];
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function ResponsiveDrawer(props: Props) {
-  const { window } = props;
+  const { window, children } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
 
   const [active, setActive] = useState(0);
@@ -67,52 +69,31 @@ export default function ResponsiveDrawer(props: Props) {
       {/* Logo */}
       <div className="w-full h-24"></div>
       <List>
-        {drawerItems.map(([icon, text], index) => {
+        {drawerItems.map(([icon, text, link], index) => {
           const isActive = index === active;
           return (
-            <ListItem
-              key={index}
-              sx={{
-                mb: 2,
-              }}
-            >
-              <ListItemButton
-                sx={{
-                  pt: 1.5,
-                  pb: 1.5,
-                  pr: 8,
-                  pl: 2.5,
-                  borderRadius: 2,
-                  bgcolor: isActive ? '#fff' : '',
-                  boxShadow: isActive ? 3 : 0,
-                }}
-                onClick={() => setActive(index)}
-              >
-                {/* <ListItemIcon sx={{ fontSize: 22, mr: -2, fontWeight: 800 }}>
-                  {icon}
-                </ListItemIcon> */}
-                <div className={`${isActive && 'font-bold'} flex space-x-6`}>
-                  <p className="text-2xl">{icon}</p>
-                  <p>{text}</p>
-                </div>
-                {/* <ListItemText
-                  primary={text}
-                  sx={{ fontSize: active ? '800' : '500' }}
-                /> */}
-              </ListItemButton>
-            </ListItem>
+            <NextLink href={link}>
+              <ListItem key={index}>
+                <ListItemButton>
+                  <ListItemIcon sx={{ fontSize: 22 }}>{icon}</ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            </NextLink>
           );
         })}
       </List>
       <Divider />
       <List>
-        {secondaryItems.map(([icon, text], index) => (
-          <ListItem key={index}>
-            <ListItemButton>
-              <ListItemIcon sx={{ fontSize: 22 }}>{icon}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
+        {secondaryItems.map(([icon, text, link], index) => (
+          <NextLink href={link}>
+            <ListItem key={index}>
+              <ListItemButton>
+                <ListItemIcon sx={{ fontSize: 22 }}>{icon}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          </NextLink>
         ))}
       </List>
     </div>
@@ -122,14 +103,14 @@ export default function ResponsiveDrawer(props: Props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex', bgcolor: '#EEF4F6' }}>
+    <Box sx={{ display: "flex", bgcolor: "#EEF4F6" }}>
       <AppBar
         position="fixed"
         elevation={0}
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
-          bgcolor: '#EEF4F6',
+          bgcolor: "#EEF4F6",
 
           // background: 'linear-gradient(to right, #cc543310, #23074d10);',
         }}
@@ -140,7 +121,7 @@ export default function ResponsiveDrawer(props: Props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <BiMenu />
           </IconButton>
@@ -161,13 +142,13 @@ export default function ResponsiveDrawer(props: Props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
+            display: { xs: "block", sm: "none" },
 
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
               width: drawerWidth,
               padding: 12,
-              bgcolor: '#EEF4F6',
+              bgcolor: "#EEF4F6",
             },
           }}
         >
@@ -176,11 +157,11 @@ export default function ResponsiveDrawer(props: Props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
               width: drawerWidth,
-              bgcolor: '#EEF4F6',
+              bgcolor: "#EEF4F6",
             },
           }}
           open
@@ -197,35 +178,7 @@ export default function ResponsiveDrawer(props: Props) {
         }}
       >
         <Toolbar />
-        <div className="h-[100vh] w-full p-8">
-          <div className="flex gap-8">
-            <div className="shadow-md bg-white rounded-md p-6 max-h-64">
-              <div className="flex gap-4">
-                <div className="p-2 bg-gray-300 rounded-full">
-                  <FcElectricity size={28} />
-                </div>{' '}
-                <p className="text-xl font-semibold">Consumption</p>
-              </div>
-
-              <p className="text-sm text-gray-600 mt-4">
-                Average Monthly Power Consumption
-              </p>
-
-              <p
-                className={`${inter.className} text-5xl font-extrabold text-blue-500 py-12`}
-              >
-                300 kWH
-              </p>
-            </div>
-            <div className="shadow-md bg-white rounded-md p-6 flex-1">
-              <p className="text-xl">
-                <FcAreaChart size={40} />
-              </p>
-
-              <Chart1 />
-            </div>
-          </div>
-        </div>
+        {children}
       </Box>
     </Box>
   );
--- pages/recommendations.tsx
+++ pages/recommendations.tsx
@@ -4,7 +4,7 @@ import { FcAreaChart, FcElectricity } from 'react-icons/fc';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function recommendations() {
   return (
     // bg-gradient-to-br from-[#cc543310] to-[#23074d10]
 
@@ -15,26 +15,11 @@ export default function Home() {
           <div className="flex gap-8">
             <div className="shadow-md bg-white rounded-md p-6 max-h-64">
               <div className="flex gap-4">
-                <div className="p-2 bg-gray-300 rounded-full">
-                  <FcElectricity size={28} />
-                </div>{' '}
                 <p className="text-xl font-semibold">Consumption</p>
               </div>
-
               <p className="text-sm text-gray-600 mt-4">
                 Average Monthly Power Consumption
               </p>
-              <p
-                className={`${inter.className} text-5xl font-extrabold text-blue-500 py-12`}
-              >
-                300 kWH
-              </p>
-            </div>
-            <div className="shadow-md bg-white rounded-md p-6 flex-1">
-              <p className="text-xl">
-                <FcAreaChart size={40} />
-              </p>
-              {/* <Chart1 /> */}    
             </div>
           </div>
         </div>
