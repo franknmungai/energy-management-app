9dfedd634de897f8bf79d51f8d2103cc5b32ebe0
Collins Kesuibai <collinskiprop13@gmail.com>
2023-10-22 08:01:51 +0300

    #[update]loads and recommendations pages

--- pages/loads.tsx
+++ pages/loads.tsx
@@ -1,27 +1,38 @@
-import PermanentDrawerLeft from '@/components/Drawer';
-import { Inter } from 'next/font/google';
-import { FcAreaChart, FcElectricity } from 'react-icons/fc';
+import { Grid, Typography, Card, CardContent } from "@mui/material";
+import PermanentDrawerLeft from "@/components/Drawer";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] });
+const loadsData = [
+  { imagePath: "/loads/images.jfif", caption: "Load Type 1" },
+  { imagePath: "/loads/images.jfif", caption: "Load Type 2" },
+  // Add more image and caption data as needed
+];
 
 export default function Loads() {
   return (
-    // bg-gradient-to-br from-[#cc543310] to-[#23074d10]
-
     <main className="bg-[#EEF4F6]">
-      {/* <Navbar /> */}
-      <PermanentDrawerLeft >
-      <div className="h-[100vh] w-full p-8">
-          <div className="flex gap-8">
-            <div className="shadow-md bg-white rounded-md p-6 max-h-64">
-              <div className="flex gap-4">
-                <p className="text-xl font-semibold">Consumption</p>
-              </div>
-              <p className="text-sm text-gray-600 mt-4">
-                Average Monthly Power Consumption
-              </p>
-            </div>
-          </div>
+      <PermanentDrawerLeft>
+        <div className="h-[100vh] w-full p-8">
+          <Typography variant="h4" sx={{mb:"8px"}}>Types of Loads</Typography>
+          <Grid container spacing={3}>
+            {loadsData.map((load, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card>
+                  <Image
+                    src={load.imagePath}
+                    alt={load.caption}
+                    width={400} // Set your desired width
+                    height={300} // Set your desired height
+                  />
+                  <CardContent>
+                    <Typography variant="subtitle1" component="div">
+                      {load.caption}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </PermanentDrawerLeft>
     </main>
--- pages/recommendations.tsx
+++ pages/recommendations.tsx
@@ -1,25 +1,44 @@
 import PermanentDrawerLeft from '@/components/Drawer';
+import { Typography } from '@mui/material';
 import { Inter } from 'next/font/google';
 import { FcAreaChart, FcElectricity } from 'react-icons/fc';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function recommendations() {
+export default function Recommendations() {
   return (
-    // bg-gradient-to-br from-[#cc543310] to-[#23074d10]
-
     <main className="bg-[#EEF4F6]">
-      {/* <Navbar /> */}
-      <PermanentDrawerLeft >
-      <div className="h-[100vh] w-full p-8">
-          <div className="flex gap-8">
-            <div className="shadow-md bg-white rounded-md p-6 max-h-64">
-              <div className="flex gap-4">
-                <p className="text-xl font-semibold">Consumption</p>
+      <PermanentDrawerLeft>
+        <div className="h-[100vh] w-full p-8">
+          <Typography variant="h4" sx={{mb:"8px"}}>Limitations and Recommendations</Typography>
+          <div className="flex flex-wrap gap-4">
+            <div className="w-full md:w-1/2 lg:w-1/3">
+              <div className="shadow-md bg-white rounded-md p-6">
+                <FcAreaChart size={48} />
+                <h2 className="text-2xl font-semibold">Limitation 1</h2>
+                <p className="text-gray-600">Description of the limitation...</p>
               </div>
-              <p className="text-sm text-gray-600 mt-4">
-                Average Monthly Power Consumption
-              </p>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3">
+              <div className="shadow-md bg-white rounded-md p-6">
+                <FcAreaChart size={48} />
+                <h2 className="text-2xl font-semibold">Limitation 2</h2>
+                <p className="text-gray-600">Description of the limitation...</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3">
+              <div className="shadow-md bg-white rounded-md p-6">
+                <FcElectricity size={48} />
+                <h2 className="text-2xl font-semibold">Recommendation 1</h2>
+                <p className="text-gray-600">Description of the recommendation...</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/3">
+              <div className="shadow-md bg-white rounded-md p-6">
+                <FcElectricity size={48} />
+                <h2 className="text-2xl font-semibold">Recommendation 2</h2>
+                <p className="text-gray-600">Description of the recommendation...</p>
+              </div>
             </div>
           </div>
         </div>
