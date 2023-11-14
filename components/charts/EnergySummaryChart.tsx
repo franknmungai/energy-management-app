f37a14d797f278d6735ae192619100699881305f
franknmungai <frankmnganga@gmail.com>
2023-11-14 13:22:17 +0300

    [#update] Chart Axes

--- components/charts/EnergySummaryChart.tsx
+++ components/charts/EnergySummaryChart.tsx
@@ -13,48 +13,52 @@ import {
 
 const data = [
   {
-    name: 'Page A',
+    name: 'Week 1',
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: 'Week 2',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: 'Week 3',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: 'Week 4',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'Week 5',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'Week 6',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: 'Week 7',
     uv: 3490,
     pv: 4300,
     amt: 2100,
   },
-];
+].map((d) => ({
+  ...d,
+  pv: d.pv / 20,
+  uv: d.uv / 20,
+}));
 
 const SimpleAreaChart = () => {
   return (
--- components/charts/UsersCharts.tsx
+++ components/charts/UsersCharts.tsx
@@ -3,48 +3,52 @@ import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const data = [
   {
-    name: 'Page A',
+    name: 'Week 1',
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: 'Week 2',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: 'Week 3',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: 'Week 4',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'Week 5',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'Week 6',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: 'Week 7',
     uv: 3490,
     pv: 4300,
     amt: 2100,
   },
-];
+].map((d) => ({
+  ...d,
+  pv: d.pv / 10,
+  uv: d.uv / 10,
+}));
 
 const UsersCharts = () => {
   return (
