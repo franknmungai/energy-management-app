358e6ec637621202e9b7ac596bfaa81e9ef2ea45
franknmungai <frankmnganga@gmail.com>
2023-11-21 18:18:30 +0300

    [#feature] recommendation panel

--- pages/recommendations.tsx
+++ pages/recommendations.tsx
@@ -13,78 +13,64 @@ export default function Recommendations() {
           <Typography variant="h5" sx={{ mb: '8px' }}>
             Recommendations
           </Typography>
-          <div className="flex flex-wrap gap-4">
-            <div className="w-full md:w-1/2 lg:w-1/3">
-              <div className="shadow-md bg-white rounded-md p-6">
-                <FcElectricity size={48} />
-                <h2 className="text-2xl font-semibold">
-                  Installation of Solar Panels
-                </h2>
-                <p className="text-gray-600">
-                  We recommend a comprehensive renovation of the library
+          <div className="grid grid-cols-3 gap-4">
+            <RecommendationPanel
+              title="Installation of Solar Panels"
+              text={`We recommend a comprehensive renovation of the library
                   infrastructure, including the installation of solar panels on
                   the roof. This renewable energy source can significantly
                   reduce the office's dependence on the grid and lower overall
-                  energy costs.
-                </p>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/3">
-              <div className="shadow-md bg-white rounded-md p-6">
-                <FcElectricity size={48} />
-                <h2 className="text-2xl font-semibold">
-                  Installation of new lighting tube fittings
-                </h2>
-                <p className="text-gray-600">
-                  Installation of new lighting tube fittings to be used with the
-                  energy saving / LED tubes.Though it may be costly now in long
-                  term it will reduce the energy consumption significantly
-                </p>
-              </div>
-            </div>
+                  energy costs.`}
+            />
 
-            <div className="w-full md:w-1/2 lg:w-1/3">
-              <div className="shadow-md bg-white rounded-md p-6">
-                <FcElectricity size={48} />
-                <h2 className="text-2xl font-semibold">
-                  Energy-Efficient Equipment
-                </h2>
-                <p className="text-gray-600">
-                  Replace old, inefficient office equipment (computers,
-                  printers, copiers) with ENERGY STAR-rated models that consume
-                  less energy during operation and have power-saving modes.
-                </p>
-              </div>
-            </div>
+            <RecommendationPanel
+              title="Installation of new lighting tube fittings"
+              text={` Installation of new lighting tube fittings to be used with the
+              energy saving / LED tubes.Though it may be costly now in long
+              term it will reduce the energy consumption significantly`}
+            />
 
-            <div className="w-full md:w-1/2 lg:w-1/3">
-              <div className="shadow-md bg-white rounded-md p-6">
-                <FcElectricity size={48} />
-                <h2 className="text-2xl font-semibold">
-                  Natural Lighting Optimization
-                </h2>
-                <p className="text-gray-600">
-                  Maximize the use of natural light through skylights, large
-                  windows, or light wells. This reduces the need for artificial
-                  lighting during the day.
-                </p>
-              </div>
-            </div>
+            <RecommendationPanel
+              title="Energy-Efficient Equipment"
+              text={`Replace old, inefficient office equipment (computers,
+                printers, copiers) with ENERGY STAR-rated models that consume
+                less energy during operation and have power-saving modes.`}
+            />
 
-            <div className="w-full md:w-1/2 lg:w-1/3">
-              <div className="shadow-md bg-white rounded-md p-6">
-                <FcElectricity size={48} />
-                <h2 className="text-2xl font-semibold">Power Management</h2>
-                <p className="text-gray-600">
-                  Encourage the use of power strips with surge protectors that
-                  can be easily switched off at the end of the day to prevent
-                  phantom energy usage from devices in standby mode.
-                </p>
-              </div>
-            </div>
+            <RecommendationPanel
+              title="Natural Lighting Optimization"
+              text={`Maximize the use of natural light through skylights, large
+              windows, or light wells. This reduces the need for artificial
+              lighting during the day.`}
+            />
+
+            <RecommendationPanel
+              title="Power Management"
+              text={`Encourage the use of power strips with surge protectors that
+              can be easily switched off at the end of the day to prevent
+              phantom energy usage from devices in standby mode.`}
+            />
           </div>
         </div>
       </PermanentDrawerLeft>
     </main>
   );
 }
+
+import React from 'react';
+
+interface Props {
+  title: string;
+  text: string;
+}
+const RecommendationPanel: React.FC<Props> = ({ title, text }) => {
+  return (
+    <div className="w-full flex-1 h-80">
+      <div className="shadow-md bg-white rounded-md p-6">
+        <FcElectricity size={48} />
+        <h2 className="text-2xl font-semibold">{title}</h2>
+        <p className="text-gray-600">{text}</p>
+      </div>
+    </div>
+  );
+};
